41f6d52bd0c0dec215b22fa36c9cb90a0dbb3ce6
Felipe Rivetti <fporto@sarrat.com.br>
2025-06-11 23:21:03 -0300

    #5: fixing modular exp logic (wip)

--- app/classes/modular_exp.py
+++ app/classes/modular_exp.py
@@ -22,52 +22,61 @@ class ModularExponentiation(BaseAlgorithm):
         self.a = a
         self.b = b
         self.n = n
+
+        self.remainders = {}
+        self.exp = None
+        self.valor_repetido = None
     
     def find_cicle(self):
         st.write("### Achando um Ciclo: ")
-        remainders = {}
 
         i = 1
         while(True):
             remainder = self.a**i % self.n
             
             # Ciclo encontrado
-            if remainder in remainders.values():
-                # st.write(remainders)
-                return i-1, remainders
+            if remainder in self.remainders.values():
+                # st.write(self.remainders)
+                # return i-1, self.remainders
+                self.exp = i-1
+                return i-1
             
             string_latex = fr"{self.a}^{{{i}}} &\equiv {remainder} \pmod{{{self.n}}} \\"
             st.latex(f"\\begin{{align*}} {string_latex} \\end{{align*}}")
 
-            remainders[i] = remainder
+            self.remainders[i] = remainder
             i+=1
 
 
     def solve(self):
-        exp, remainders = self.find_cicle()
-        valor_repetido = remainders[exp]
-        # st.write(remainders)
+        # exp, remainders = self.find_cicle()
+        # exp = self.find_cicle()
+
+        # valor_repetido = self.remainders[exp]
+        self.valor_repetido = self.remainders[self.exp]
+
+        # st.write(self.remainders)
         # st.write(f"### Ciclo encontrado: {exp} e valor repetido: {valor_repetido}")
 
         # Prevenção contra exp = 0, embora find_cicle deva retornar >= 1 se n>=1
-        if exp == 0:
+        if self.exp == 0:
             st.error("Erro: 'exp' calculado como zero, verifique a função find_cicle e os inputs.")
             return
 
-        quotient = self.b // exp
-        remainder = self.b % exp
-        modular_result = pow(self.a, remainder, self.n)
+        quotient = self.b // self.exp
+        remainder = self.b % self.exp
+        # modular_result = pow(self.a, remainder, self.n)
 
-        self.display_results(quotient, remainder, exp, modular_result, valor_repetido, remainders)
+        self.display_results(quotient, remainder, self.exp)
 
 
-    def display_results(self, quotient, remainder, exp, modular_result, valor_repetido, remainders):
+    def display_results(self, quotient, remainder, exp):
         string_latex = fr"""
         \begin{{alignat*}}{{2}}
         {self.a}^{{{self.b}}} &\equiv ({self.a}^{{{exp}}})^{{{quotient}}} \cdot {self.a}^{{{remainder}}} && \pmod{{{self.n}}} \\
-        {self.a}^{{{self.b}}} &\equiv ({{{valor_repetido}}})^{{{quotient}}} \cdot {self.a}^{{{remainder}}} && \pmod{{{self.n}}} \\
-        {self.a}^{{{self.b}}} &\equiv ({{{valor_repetido ** quotient}}}) \cdot {self.a}^{{{remainder}}} && \pmod{{{self.n}}} \\
-        {self.a}^{{{self.b}}} &\equiv {(valor_repetido ** quotient * remainders[remainder])%self.n} && \pmod{{{self.n}}}
+        {self.a}^{{{self.b}}} &\equiv ({{{self.valor_repetido}}})^{{{quotient}}} \cdot {self.a}^{{{remainder}}} && \pmod{{{self.n}}} \\
+        {self.a}^{{{self.b}}} &\equiv ({{{self.valor_repetido ** quotient}}}) \cdot {self.a}^{{{remainder}}} && \pmod{{{self.n}}} \\
+        {self.a}^{{{self.b}}} &\equiv {(self.valor_repetido ** quotient * self.remainders[remainder])%self.n} && \pmod{{{self.n}}}
         \end{{alignat*}}
         """
 
