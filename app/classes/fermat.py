5a534a18207e9507198e1d4dda7640afd6773fb0
Felipe Rivetti <fporto@sarrat.com.br>
2025-06-13 18:54:30 -0300

    #3: optimizations in fermat algorithm and improving display_results

--- app/classes/fermat.py
+++ app/classes/fermat.py
@@ -6,14 +6,16 @@ import streamlit as st
 from classes.base_algorithm import BaseAlgorithm, classproperty
 
 
-
 class AlgoritmoDeFermat(BaseAlgorithm):
 
     @classproperty
     def input_format(cls):
         return r"\text{Aplica o Algoritmo de Fermat para } N"
     
-    params = ["n"]
+    @classproperty
+    def params(cls):
+        params = ["n"]
+        return params
 
     @staticmethod
     def validate_input(input):
@@ -22,50 +24,56 @@ class AlgoritmoDeFermat(BaseAlgorithm):
             if input <= 0:
                 st.error("Por favor, digite um número inteiro maior que zero.")
                 return False
+            if input%2 == 0:
+                st.error("Por favor, digite um número inteiro ímpar.")
+                return False
             return True
+        
         except ValueError:
             st.error("Por favor, digite um número inteiro válido.")
             return False
 
-
     def __init__(self, n):
         self.n = n
         self.results = {}
-
-    def display_results(self, df, x, y):
-        if (x == (self.n+1)/2):
-            st.write(f"## {self.n} é primo")
+    
+    def display_results(self, values):
+        if (self.results["factor1"] == 1 or self.results["factor2"] == 1):
+            st.latex(rf"\LARGE {self.n} \text{{ é primo}}")
         else:
-            st.write(f"## Fator 1: {x-y} | Fator 2: {x+y}")
+            st.latex(rf"\LARGE 1^\circ \text{{ Fator}}: {self.results['factor1']} \quad \mid \quad 2^\circ \text{{ Fator}}: {self.results['factor2']}")
+
+        df = pd.DataFrame(values, columns=['n', 'x', 'y', 'z'])
         st.write(df)
+        
 
     def fermat_algorithm(self):
-        df = pd.DataFrame(columns=['n', 'x', 'y', 'z'])
-
-        x = math.floor(math.sqrt(self.n))
+        x = math.isqrt(self.n)
+        x_squared = x * x
         y = 0
-        z = self.n - x**2 + y**2
-        df.loc[0] = [self.n, x, 0, z]
+        z = self.n - x_squared + y**2
 
-        line = 0
-        while(df.iloc[line, 3] != 0):
-            line += 1
-            x = df.iloc[line-1, 1] + 1
-            y = math.floor(math.sqrt(x**2 - self.n))
-            z = self.n - x**2 + y**2
-            df.loc[line] = ['-', x, y, z]
+        values = []
+        values.append((self.n, x, y, z))
 
-
-        x = df.iloc[line, 1]
-        y = df.iloc[line, 2]
-
-        self.display_results(df, x, y)
+        while z != 0:
+            x += 1
+            x_squared += 2*x - 1
+            diff = x_squared - self.n
+            if diff < 0:
+                continue  # ou break, dependendo do contexto
+            y = math.isqrt(diff)
+            z = self.n - x_squared + y**2
+            values.append(('-', x, y, z))
 
         self.results = {
             "factor1": x - y,
             "factor2": x + y,
         }
-    
+
+        return values
+
     def solve(self):
-        self.fermat_algorithm()
-        return self.results
+        values = self.fermat_algorithm()
+        self.display_results(values)
+        # return self.results
